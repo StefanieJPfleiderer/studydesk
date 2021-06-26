b185b4499fb900fd72d09d613dd16c212d66df74
Stefanie <s.j.pfleiderer@web.de>
2021-06-26 17:37:51 +0200

    Add script to create database and table for categories

---
+++ studydesk.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS studydesk;
+
+CREATE DATABASE studydesk;
+USE studydesk;
+
+CREATE TABLE category (
+    ID INT(6) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    image LONGBLOB);
+
+# Username and password removed for security reasons
+GRANT ALL PRIVILEGES ON studydesk.* TO ''@'localhost'
+IDENTIFIED BY '' WITH GRANT OPTION;
